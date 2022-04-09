eb0150bb72dffb25874a74843ef6f6788af6882f
Jan Lindauer <lindauer56@gmail.com>
2022-04-10 00:35:58 +0200

    Base functionality

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,6 @@ import {showEditor} from "./view/editor.js"
 
 
 const wrapper = document.querySelector(".wrapper");
-let content = "<h1>Hello world</h1>This is my editor"
+let content = "<h1 id='ID'>Hello world</h1> This is my editor";
 showEditor(wrapper, content);
 setSidebarItems(["Item 1", "Item 2","Item 3", "Item 4"]);
--- js/view/editor.js
+++ js/view/editor.js
@@ -1,22 +1,159 @@
+function getCurrentNode(){
+    const paper = document.querySelector(".paper");
+    paper.focus();
+    let currentNode = document.getSelection().anchorNode.parentNode
+    if (currentNode === paper){
+        currentNode = document.getSelection().anchorNode;
+    }
+    return currentNode;
+}
+
+function insertElement(newNode){
+    const currentNode = getCurrentNode();
+    console.log(currentNode)
+    currentNode.parentNode.insertBefore(newNode, currentNode.nextSibling);
+}
+
+function addPicture(){
+    const picture = document.createElement("img");
+    picture.src = "https://d32-a.sdn.cz/d_32/c_static_QR_Q/rZ59K/media/img/logo_v2.svg";
+    insertElement(picture);
+}
+
+function addTitle(level){
+    const title = document.createElement(`h${level}`);
+    title.innerText = "Hello";
+    insertElement(title);
+    title.focus();
+}
+
+
 
 
 const functions = [
     {
         title: "Bold",
         action: "bold",
-        icon: "https://img.icons8.com/fluency-systems-filled/48/000000/bold.png",
-        callback: (e)=>{
-
-        }
+        icon: "bold"
     },
-
-
+    {
+        title: "Italic",
+        action: "italic",
+        icon: "italic"
+    },
+    {
+        title: "Underline",
+        action: "underline",
+        icon: "underline"
+    },
+    {
+        title: "Strike through",
+        action: "strikeThrough",
+        icon: "strikethrough"
+    },
+    {
+        title: "Justify Left",
+        action: "justifyLeft",
+        icon: "align-left"
+    },
+    {
+        title: "Justify Center",
+        action: "justifyCenter",
+        icon: "align-center"
+    },
+    {
+        title: "Justify right",
+        action: "justifyRight",
+        icon: "align-right"
+    },
+    {
+        title: "Format Block",
+        action: "formatBlock",
+        icon: "align-justify"
+    },
+    {
+        title: "Insert Ordered List",
+        action: "insertOrderedList",
+        icon: "numbered-list"
+    },
+    {
+        title: "Insert Unordered List",
+        action: "insertUnorderedList",
+        icon: "bulleted-list"
+    },
+    {
+        title: "Italic",
+        action: "italic",
+        icon: "italic"
+    },
+    {
+        title: "Outdent",
+        action: "outdent",
+        icon: "outdent"
+    },
+    {
+        title: "Indent",
+        action: "indent",
+        icon: "indent"
+    },
+    {
+        title: "Insert Horizontal Rule",
+        action: "insertHorizontalRule",
+        icon: "horizontal-line"
+    },
+    {
+        title: "Undo",
+        action: "undo",
+        icon: "undo--v1"
+    },
+    {
+        title: "Remove Format",
+        action: "removeFormat",
+        icon: "remove-format"
+    },
+    {
+        title: "Create Link",
+        action: "createLink",
+        icon: "add-link"
+    },
+    {
+        title: "Delete link",
+        action: "unlink",
+        icon: "delete-link"
+    },
+    {
+        title: "Add picture",
+        callback: addPicture,
+        icon: "picture--v1"
+    },
+    {
+        title: "Add level 1 title",
+        callback: ()=>addTitle(1),
+        icon: "header-1"
+    },
+    {
+        title: "Add level 2 title",
+        callback: ()=>addTitle(2),
+        icon: "header-2"
+    },
+    {
+        title: "Add level 3 title",
+        callback: ()=>addTitle(3),
+        icon: "header-3"
+    }
 ];
 
 
+let lastFocused = undefined;
+
 function executeAction(){
-    let action = this.dataset.action;
+    let action = this.getAttribute("action");
+    console.log(action);
     document.execCommand(action, false);
+    const paper = document.querySelector(".paper");
+    paper.focus();
+    console.log(lastFocused)
+    console.log(document.getSelection().anchorNode.parentNode)
 }
 
 
@@ -32,14 +169,16 @@ function showTools(toolsBox){
             tool.addEventListener("click", executeAction);
         }
         tool.title = func.title;
+        tool.setAttribute("action", func.action);
 
         //add icon
         const icon = document.createElement("img");
         if (func.icon !== undefined){
-            icon.src = `https://img.icons8.com/fluency-systems-filled/48/000000/${func.action}.png`;
-        }else{
-            icon.src = func.icon;
+            icon.src = `https://img.icons8.com/fluency-systems-filled/48/000000/${func.icon}.png`;
+        }else if (func.iconLink !== undefined) {
+            icon.src = func.iconLink;
         }
+        icon.alt = func.action;
         tool.append(icon);
 
         toolsBox.append(tool);
@@ -58,6 +197,11 @@ export function showEditor(where, content){
     paper.innerHTML = content;
     paper.setAttribute("contenteditable", "true")
     paper.className = "paper";
+
+    //track last visited child
+    paper.childNodes.forEach((child)=>{
+        child.addEventListener("focus", ()=>{lastFocused=child;})
+    });
 
 
     editor.append(tools,paper);
